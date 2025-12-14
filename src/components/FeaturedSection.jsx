f5c91a5a281e87a33adae8559c1d5147a2826c31
BALAKRISHNAN <balakrish007007@gmail.com>
2025-12-14 23:46:38 +0530

    create movie cards

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -1,27 +1,37 @@
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { BlurCircle } from "./BlurCircle";
+import { dummyShowsData } from "../assets/assets";
+import { MovieCard } from "./MovieCard";
 
-export function FeaturedSection(){
+export function FeaturedSection() {
   const navigate = useNavigate();
-  return(
+
+  return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
       <div className="relative flex items-center justify-between pt-20 pb-10">
-        <BlurCircle top='0' right='-80px'/>
+        <BlurCircle top='0' right='-80px' />
         <p className="text-gray-300 font-medium text-lg">Now Showing</p>
-        <button onClick={()=>{
+        <button onClick={() => {
           navigate('/movies')
         }} className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer">
-          View All 
-          <ArrowRight className="group-hover:translate-x-0.5 transition w-4.5 h-4.5"/>
+          View All
+          <ArrowRight className="group-hover:translate-x-0.5 transition w-4 h-4" />
         </button>
       </div>
-      <div>
 
-      </div>
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+  {dummyShowsData.slice(0, 4).map((show) => (
+    <MovieCard key={show.id} movie={show} />
+  ))}
+</div>
+
+
+
+
       <div className="flex justify-center mt-20">
-        <button className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer" onClick={()=>{
-          navigate('/movies');scrollTo(0,0)
+        <button className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer" onClick={() => {
+          navigate('/movies'); scrollTo(0, 0)
         }}>Show more</button>
       </div>
     </div>
