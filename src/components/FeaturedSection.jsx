5f0d319a27b9d6193ba434435062ec9dc24eadc5
BALAKRISHNAN <balakrish007007@gmail.com>
2025-12-17 12:10:01 +0530

    created trailers section and completed footer

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -12,9 +12,7 @@ export function FeaturedSection() {
       <div className="relative flex items-center justify-between pt-20 pb-10">
         <BlurCircle top='0' right='-80px' />
         <p className="text-gray-300 font-medium text-lg">Now Showing</p>
-        <button onClick={() => {
-          navigate('/movies')
-        }} className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer">
+        <button onClick={() => navigate('/movies')} className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer">
           View All
           <ArrowRight className="group-hover:translate-x-0.5 transition w-4 h-4" />
         </button>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { FeaturedSection } from "../components/FeaturedSection";
 import { HeroSection } from "../components/HeroSection";
+import { TrailersSection } from "../components/TrailersSection";
 
 export function HomePage(){
   return (
     <>
       <HeroSection/>
       <FeaturedSection/>
+      <TrailersSection />
     </>
   )
 }
