ed7db43eac3c67372b565e75435fa20e3a8290ce
BALAKRISHNAN <balakrish007007@gmail.com>
2025-12-24 10:09:41 +0530

    completed movie details page

--- src/components/DateSelect.jsx
+++ src/components/DateSelect.jsx
@@ -1,7 +1,22 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { BlurCircle } from "./BlurCircle";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 export function DateSelect( {dateTime, id} ) {
+
+  const [ selected,setSelected ] = useState(null);
+  const navigate = useNavigate();
+
+  const onBookHandler = () => {
+    if(!selected){
+      return toast('Please select the date');
+    }
+    navigate(`/movies/${id}/${selected}`);
+    scrollTo(0,0);
+  }
+
   return (
     <div id="dateSelect" className="pt-30">
       <div className="flex flex-col md:flex-row items-center justify-between gap-10 relative p-8 bg-primary/10 border border-primary/20 rounded-lg">
@@ -13,7 +28,9 @@ export function DateSelect( {dateTime, id} ) {
             <ChevronLeftIcon width={28}/>
             <span className="grid grid-cols-3 md:flex flex-wrap md:max-w-lg gap-4">
               {Object.keys(dateTime).map((date)=>(
-                <button key={date} className="flex flex-col items-center justify-between h-14 w-14 aspect-square rounded cursor pointer">
+                <button onClick={() => {
+                  setSelected(date);
+                }} key={date} className={`flex flex-col items-center justify-between h-14 w-14 aspect-square rounded cursor pointer ${selected === date ? "bg-primary text-white" : "border border-primary/70"}`}>
                   <span>{new Date(date).getDate()}</span>
                   <span>{new Date(date).toLocaleDateString("en-US",{month: "short"})}</span>
                 </button>
@@ -22,7 +39,7 @@ export function DateSelect( {dateTime, id} ) {
             <ChevronRightIcon width={28}/>
           </div>
         </div>
-        <button className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer">Book Now</button>
+        <button onClick={onBookHandler} className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer">Book Now</button>
       </div>
     </div>
   )
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import { BlurCircle } from "../components/BlurCircle";
 import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
 import formatRuntime from "../lib/formatRunTime";
 import { DateSelect } from "../components/DateSelect";
+import { MovieCard } from "../components/MovieCard"
 
 export function MoviesDetails() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [show, setShow] = useState(null);
 
   const getShow = async () => {
     const show = dummyShowsData.find(show => show._id === id);
-    setShow({
+    if(show){
+      setShow({
       movie: show,
       dateTime: dummyDateTimeData
     });
+    }
+    
   }
 
   useEffect(() => {
@@ -74,6 +79,16 @@ export function MoviesDetails() {
       
       <DateSelect dateTime={show.dateTime} id={id}/>
 
+      <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+          {dummyShowsData.slice(0,4).map( (movie) => (
+            <MovieCard key={movie._id} movie={movie} />
+          ))}
+      </div >
+
+      <div className="flex justify-center mt-20">
+          <button onClick={() => {navigate('/movies'); scrollTo(0,0)} } className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">Show more</button>
+      </div>
 
     </div>
   ) : (
