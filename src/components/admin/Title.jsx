1666f67f177c5d56d6614bb0aadec01959b0f5cc
BALAKRISHNAN <balakrish007007@gmail.com>
2025-12-29 07:41:49 +0530

    created admin dashboard

---
+++ src/components/admin/Title.jsx
@@ -0,0 +1,7 @@
+export function Title({ text1,text2 }) {
+  return (
+    <h1 className="font-mdium text-2xl">
+      {text1} <span className="underline text-primary">{text2}</span>
+    </h1>
+  )
+}
